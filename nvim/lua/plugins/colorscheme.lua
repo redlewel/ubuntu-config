ce0a78ff91e0ba90a40017fb970af787bfb5b85e
Eric Lewellen <elewellen@hivelocity.net>
2026-02-27 20:41:36 -0600

    initial dotfiles commit from work pc

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,32 @@
+return {
+{
+  "scottmckendry/cyberdream.nvim",
+  opts = {
+	transparent = false,
+	theme = {
+	  variant = "default",
+	},
+  },
+},
+{
+  "EdenEast/nightfox.nvim",
+  opts = {
+	transparent = false,
+	theme = {
+	  variant = "default",
+	},
+  },
+},
+{ 'dasupradyumna/midnight.nvim', lazy = false, priority = 1000 },
+{
+  "ficcdaf/ashen.nvim",
+  lazy = false,
+  priority = 1000
+},
+{
+  "LazyVim/LazyVim",
+  opts = {
+	colorscheme = "midnight",
+  }
+}
+}
